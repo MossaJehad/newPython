0c56ab416d3f5a45e15ae4ddb77cfc42cc418837
MossaJehad <Moosa.Jehad65@gmail.com>
2025-06-17 19:28:04 +0300

    new

---
+++ 1day.py
@@ -0,0 +1,31 @@
+import datetime
+x, y, i = input("Enter your name: "), input("Enter your age: "), 0
+
+try:
+    y = int(y)
+except ValueError:
+    print("Please enter a valid number")
+    exit()
+
+if(y < 0):
+    print("Please enter number bigger than zero")
+    exit()
+
+while(i < len(x) and 'z' >= x[i] >= 'a'):
+    i += 1
+ 
+if(i != len(x) or len(x) == 0):
+    print("Your name must contain string")
+    exit()
+
+year = datetime.datetime.now().year
+year100at = int(year) + (100 - y)
+
+print("Hi", x, end='')
+
+if(y < 100):
+    print(", your will turn 100 in the year", year100at)
+    exit()
+else:
+    print(", you alread turned 100 at the year", year100at)
+
