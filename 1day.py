be6f20a7bb38301dc087b5502c825d91f687b123
MossaJehad <Moosa.Jehad65@gmail.com>
2025-06-17 20:13:25 +0300

    fix: Uppercase charcters for the name, Adding comments to each section, And fixing the overflow number for the age

--- 1day.py
+++ 1day.py
@@ -1,15 +1,7 @@
 import datetime
 x, y, i = input("Enter your name: "), input("Enter your age: "), 0
 
-try:
-    y = int(y)
-except ValueError:
-    print("Please enter a valid number")
-    exit()
-
-if(y < 0):
-    print("Please enter number bigger than zero")
-    exit()
+### CHECK THE NAME ###
 
 while(i < len(x) and (('z' >= x[i] >= 'a') or ('Z' >= x[i] >= 'A'))):
     i += 1
@@ -17,6 +9,26 @@ while(i < len(x) and (('z' >= x[i] >= 'a') or ('Z' >= x[i] >= 'A'))):
 if(i != len(x) or len(x) == 0):
     print("Your name must contain string")
     exit()
+
+
+### CHECK THE AGE ###
+
+try:
+    y = int(y)
+except ValueError:
+    print("Please enter a valid number")
+    exit()
+
+if(y > 120)
+    print("You either broke the world record or just being silly")
+    exit()
+
+if(y < 0):
+    print("Please enter number bigger than zero")
+    exit()
+
+
+### CALCULATE AND PRINT ###
 
 year = datetime.datetime.now().year
 year100at = int(year) + (100 - y)
