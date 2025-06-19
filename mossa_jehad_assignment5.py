cfca0d64d9c581d5afabf11ea13c39a74e35d0d0
MossaJehad <moosa.jehad65@gmail.com>
2025-06-19 19:41:14 +0300

    feat: Now it print the whole dictonary.

--- mossa_jehad_assignment5.py
+++ mossa_jehad_assignment5.py
@@ -14,3 +14,10 @@ onlineStore = {
 # In dictonaries you can add a dictonary inside another
 
 print(onlineStore["Mouse"])
+
+for prod, price in onlineStore.items():
+    if (type(price) == dict):
+        for subProd, subPrice in price.items():
+             print(f" • Product: {subProd} --- Price: ${subPrice}")
+    else:
+        print(f"Product: {prod} --- Price: ${price}")
