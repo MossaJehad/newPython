2c0e7163ed7e688372fb1b91c078529106cf2d44
Mossa Jehad <50895915+MossaJehad@users.noreply.github.com>
2025-07-15 11:11:21 +0300

    latest

--- mossa_jehad_assignment20.py
+++ mossa_jehad_assignment20.py
@@ -48,13 +48,18 @@ print(df)
 df = df.drop('Name', axis = 1)
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 X = df.drop('Survived', axis = 1)
 y = df['Survived']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-model = SVC(kernel="linear", C=75)
+model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
 
-print(classification_report(y_test, model.predict(X_test)))
+y_pred = model.predict(X_test)
+
+print("Classification Report: \n", classification_report(y_test, y_pred))
+
+print(f"Accuracy Score: %{accuracy_score(y_test, y_pred) * 100:.4f}")
